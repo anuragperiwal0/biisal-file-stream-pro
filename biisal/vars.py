d7e884328f147cadff8d01ba5ea6955aecd34bad
Thor112274 <126616021+Thor112274@users.noreply.github.com>
2024-08-03 00:03:23 +0530

    Update vars.py

--- biisal/vars.py
+++ biisal/vars.py
@@ -6,24 +6,24 @@ from dotenv import load_dotenv
 
 
 load_dotenv()
-bot_name = "Bɪɪsᴀʟ Fɪʟᴇ2Lɪɴᴋ Bᴏᴛ"
+bot_name = "Mʟ Fɪʟᴇ ᴛᴏ Lɪɴᴋ Bᴏᴛ"
 bisal_channel = "https://telegram.me/Movie_loverzz"
-bisal_grp = "https://t.me/+FLXYlsb037cwNzE1"
+bisal_grp = "https://t.me/+Ld8N-bvIbCJjODhl"
 
 class Var(object):
     MULTI_CLIENT = False
-    API_ID = int(getenv('API_ID', ''))
-    API_HASH = str(getenv('API_HASH', ''))
-    BOT_TOKEN = str(getenv('BOT_TOKEN' , ''))
+    API_ID = int(getenv('API_ID', '27258953'))
+    API_HASH = str(getenv('API_HASH', '0add43fc460daca0a86077989cfc414f'))
+    BOT_TOKEN = str(getenv('BOT_TOKEN' , '7243323940:AAHO12ALIVc3c6vJ5jC0wTFeQjsfdMbQ0Z4'))
     name = str(getenv('name', 'Mlfiletolinkbot'))
     SLEEP_THRESHOLD = int(getenv('SLEEP_THRESHOLD', '60'))
     WORKERS = int(getenv('WORKERS', '4'))
-    BIN_CHANNEL = int(getenv('BIN_CHANNEL', ''))
-    NEW_USER_LOG = int(getenv('NEW_USER_LOG', ''))
+    BIN_CHANNEL = int(getenv('BIN_CHANNEL', '-1001747292996'))
+    NEW_USER_LOG = int(getenv('NEW_USER_LOG', '-1001747292996'))
     PORT = int(getenv('PORT', '8080'))
     BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '0.0.0.0'))
     PING_INTERVAL = int(environ.get("PING_INTERVAL", "1200"))  # 20 minutes
-    OWNER_ID = [int(x) for x in os.environ.get("OWNER_ID", "5397984467").split()]
+    OWNER_ID = [int(x) for x in os.environ.get("OWNER_ID", "949657126").split()]
     NO_PORT = bool(getenv('NO_PORT', False))
     APP_NAME = None
     OWNER_USERNAME = str(getenv('OWNER_USERNAME', 'TonYStarkk616'))
